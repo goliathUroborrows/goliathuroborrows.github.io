ca9f06250be8c1767a5c055e7691b0461c83b315
goliathUroborrows <djiwoonyi@gmail.com>
2014-02-25 17:56:25 -0800

    Editing Clicks
    I don’t even know whats going on anymore

--- Clicks/clicks.js
+++ Clicks/clicks.js
@@ -1,8 +1,45 @@
 /*global $*/
 /*jslint sloppy:true, browser: true*/
-$("h1.page-title").text("Hello World!");
+/*$("h1.page-title").text("Hello World!");
 $(window).on("click", function (e) {
+    $("h1.page-title").text(e.pageX);
     $("img.logo").css({left: e.clientX,
                        top: e.clientY});
-    $("img.logo").toggleClass("hide");
+    $("img.logo").toggleClass("rotated");
 });
+function randomMargin() {
+    return Math.random() * 100 - 90;
+}
+*/
+var vx = Math.random() * 10 - 5;
+var vy = Math.random() * 10 - 5;
+var x = $(window).width / 2;
+var y = $(window).height / 2;
+var theta = 0;
+var vtheta = Math.PI / 180;
+var r = 60;
+setInterval(function () {
+    $("img.logo").css({
+        "left": x + r * Math.cos(theta),
+        "top": y + r * Math.sin(theta)
+    });
+    theta = theta + vtheta;
+    /*
+    x = x + vx;
+    y = y + vy;
+    if (x < 0) {
+        x = 0;
+        vx = -vx;
+    } else if (x > $(window).width()) {
+        x = $(window).width();
+        vx = -vx;
+    }
+    if (y < 0) {
+        y = 0;
+        vy = -vy;
+    } else if (y > $(window).height()) {
+        y = $(window).height();
+        vy = -vy;
+    }
+, 20); 
+*/
