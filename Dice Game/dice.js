b35036de896be5f8f8f4485aec841e36539f0754
goliathUroborrows <djiwoonyi@gmail.com>
2014-02-27 18:03:16 -0800

    dice game eidit
    wahty

--- Dice Game/dice.js
+++ Dice Game/dice.js
@@ -24,3 +24,85 @@ For extra challenge, here are some ideas:
 * Implement another dice game, such as Farkle
 
 */
+
+function restartGame() {
+    // TODO: add the class 'selectable' to every element
+    //       with the 'die' class
+    // TODO: remove the class 'hold' from every element
+    //       with the 'die' class
+    $('.die').addClass('selectable');
+    $('.die').removeClass('hold');
+    rollDice();
+}
+
+function randomRoll() {
+    // return a random number between 1 and 6
+    return 1 + Math.floor(6 * Math.random(1, 6));
+}
+
+function calculateScore() {
+    // Calculate the total and determine if it qualifies
+    var has1 = false;
+    var has4 = false;
+    var total = 0;
+    $('.die.hold').each(function () {
+        var roll = parseFloat($(this).attr('data-roll'));
+        if (!has1 && roll === 1) {
+            has1 = true;
+        } else if (!has4 && roll === 4) {
+            has4 = true;
+        } else {
+            total = total + roll;
+        }
+    });
+    return {qualify: has1 && has4, value: total};
+}
+
+function updateState() {
+    // Enable or disable the roll button
+    if ($('.die.hold.selectable').length > 0) {
+        $('button.roll').removeAttr('disabled');
+    } else {
+        $('button.roll').attr('enabled');
+    }
+    var score = calculateScore();
+    $('.score')
+        .toggleClass('invalid', !score.qualify)
+        .text(score.value);
+    var rollAgain = $('.die:not(.hold)').length > 0;
+    $('button.roll').toggleClass('hide', !rollAgain);
+    $('button.play-again').toggleClass('hide', rollAgain);
+}
+
+function rollDice() {
+    // Roll all of the dice
+    // TODO: remove the 'selectable' class from all elements that have both
+    //       the 'die' and 'hold' class
+    // TODO: set the 'data-roll' attribute to the randomRoll function
+    //       for all elements that have the 'die' and 'selectable' class
+    updateState();
+    if ($('.die.hold.selectable')) {
+        $('.die.hold').removeClass('selectable');
+    } else {
+        $('.die.selectable');
+    }
+    if ($('.die.selectable')) {
+        $('.die').Attr('data-roll');
+    }
+}
+
+function holdDie() {
+    // Hold a single die
+    var die = $(this);
+    // TODO: toggle the class 'hold' for the die
+    die.toggleClass('hold');
+    updateState();
+}
+
+$(document).ready(function () {
+    // This gets called exactly once to set everything up
+    $('.dice').on('click', '.die.selectable', holdDie);
+    $('.buttons').on('click', 'button.roll:enabled', rollDice);
+    $('.buttons').on('click', 'button.play-again:enabled', restartGame);
+    restartGame();
+});
