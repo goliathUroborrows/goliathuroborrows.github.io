99558c2d0f9c0784c6da6840d728ef8cbab5eac7
goliathUroborrows <djiwoonyi@gmail.com>
2014-02-27 18:02:46 -0800

    Dice Game edit
    what is even going on

---
+++ Dice Game/dice.js
@@ -0,0 +1,26 @@
+/*global $*/
+/*jslint sloppy:true, browser: true, devel: true*/
+
+/*
+
+References:
+
+* http://api.jquery.com/addClass/
+* http://api.jquery.com/removeClass/
+* http://api.jquery.com/toggleClass/
+* http://api.jquery.com/attr/
+* https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Getting_started/Selectors
+
+Tips:
+
+1. Start with the restartGame function
+2. Move on to the roll function
+3. Finally, add the holdDie function
+
+For extra challenge, here are some ideas:
+
+* Make it look better!
+* Animate the dice for each roll https://api.jquery.com/animate/
+* Implement another dice game, such as Farkle
+
+*/
